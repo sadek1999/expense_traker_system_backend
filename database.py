6877234e395b614bb39873e9d363c2d4b1e15760
sadek1999 <sadekali1530@gmail.com>
2026-08-25 00:15:20 +0600

    successfully connect with postgras db

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 SQLALCHEMY_DATABASE_URL='postgresql://postgres.lvcvhsculjbffuzdqhlb:Expences#test@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
-engine=create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 sessionLocal = sessionmaker (autoflush=False, autocommit=False,bind= engine)
 
 Base=declarative_base()
